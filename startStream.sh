d5bb2a501415ac7da84adbc3c31377d9d6c86d0e
Xabier Dendarieta <xabier.dendarieta@unavarra.es>
2020-05-27 20:04:07 +0200

    Arreglos en scripts

--- startStream.sh
+++ startStream.sh
@@ -1,4 +1,5 @@
 rm -r -f "src/output/more/live/"
+mkdir "src/output/more/live/"
 
-ffmpeg -i "src/output/more/live.mp4" -vf scale=640:480 -f hls -c:v h264 -profile:v baseline -pix_fmt yuv420p -hls_time 1 -strict experimental "src/output/more/live/stream.m3u8"
-ffmpeg -i "src/output/more/live.mp4" -vf scale=640:480 -f hls -c:v h264 -profile:v baseline -pix_fmt yuv420p -hls_time 1 -strict experimental "src/output/more/live/stream.m3u8"
+ffmpeg -i "src/output/more/live.mp4" -vf scale=640:480 -f hls -c:v h264 -profile:v baseline -pix_fmt yuv420p -hls_time 5 -strict experimental "src/output/more/live/stream.m3u8"
+ffmpeg -i "src/output/more/live.mp4" -vf scale=640:480 -f hls -c:v h264 -profile:v baseline -pix_fmt yuv420p -hls_time 5 -strict experimental "src/output/more/live/stream.m3u8"
