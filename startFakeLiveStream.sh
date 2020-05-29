8c7336a89cecb303c4a9b3bb00b916601b2afa24
Xabier Dendarieta <xabier.dendarieta@unavarra.es>
2020-05-29 15:13:13 +0200

    Pequeño arreglo en startFakeLiveStream

--- startFakeLiveStream.sh
+++ startFakeLiveStream.sh
@@ -1,5 +1,4 @@
 rm -r -f "src/output/more/fake/"
 mkdir "src/output/more/fake/"
 
-ffmpeg -i "src/output/more/fake_live.mp4" -vf scale=640:480 -f hls -c:v h264 -profile:v baseline -pix_fmt yuv420p -hls_time 5 -strict experimental "src/output/more/fake/stream.m3u8"
-ffmpeg -i "src/output/more/fake_live.mp4" -vf scale=640:480 -f hls -c:v h264 -profile:v baseline -pix_fmt yuv420p -hls_time 5 -strict experimental "src/output/more/fake/stream.m3u8"
+ffmpeg -i "src/output/more/fake_live.mp4" -vf scale=640:480 -f hls -c:v h264 -profile:v baseline -pix_fmt yuv420p -hls_time 1 -strict experimental "src/output/more/fake/stream.m3u8"
